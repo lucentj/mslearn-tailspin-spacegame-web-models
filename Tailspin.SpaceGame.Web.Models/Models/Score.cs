3bb7db5289c88e2594299c790b8f7c5ffd7540b2
John Lucenta <John.Lucenta@Accenture.com>
2020-05-22 16:14:44 -0500

    Add GameStyle property

--- Tailspin.SpaceGame.Web.Models/Models/Score.cs
+++ Tailspin.SpaceGame.Web.Models/Models/Score.cs
@@ -19,5 +19,33 @@ namespace TailSpin.SpaceGame.Web.Models
         // The game region (map) the score is associated with.
         [JsonProperty(PropertyName = "gameRegion")]
         public string GameRegion { get; set; }
+
+        using Newtonsoft.Json;
+
+namespace TailSpin.SpaceGame.Web.Models
+{
+    public class Score : Model
+    {
+        // The ID of the player profile associated with this score.
+        [JsonProperty(PropertyName = "profileId")]
+        public string ProfileId { get; set; }
+
+        // The score value.
+        [JsonProperty(PropertyName = "score")]
+        public int HighScore { get; set; }
+
+        // The game mode the score is associated with.
+        [JsonProperty(PropertyName = "gameMode")]
+        public string GameMode { get; set; }
+
+        // The game region (map) the score is associated with.
+        [JsonProperty(PropertyName = "gameRegion")]
+        public string GameRegion { get; set; }
+
+        // The game style (difficulty) the score is associated with.
+        [JsonProperty(PropertyName = "gameStyle")]
+        public string GameStyle { get; set; }
+    }
+}
     }
 }
